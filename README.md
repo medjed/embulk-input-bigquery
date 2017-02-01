9329d71ff7ff5513a041e3123d0ea74d8df3faa5
Takeru Narita <narita_takeru@Takerus-MacBook-Air.local>
2017-02-01 16:39:58 +0900

    add sql_erb

--- README.md
+++ README.md
@@ -22,3 +22,20 @@ in:
 out:
   type: stdout
 ```
+
+If, table name is changeable, then
+
+```
+in:
+  type: bigquery
+  project: 'project-name'
+  keyfile: '/home/hogehoge/bigquery-keyfile.json'
+  sql: 'SELECT price,category_id FROM [ecsite.products_<%= params["date"].strftime("%Y%m")  %>] GROUP BY category_id'
+  erb_params:
+    date: "require 'date'; (Date.today - 1)"
+  columns:
+    - {name: price, type: long}
+    - {name: category_id, type: long}
+    - {name: month, type: timestamp, format: '%Y-%m', eval: 'require "time"; Time.parse(params["date"]).to_i'}
+```
+
--- lib/embulk/input/bigquery.rb
+++ lib/embulk/input/bigquery.rb
@@ -1,5 +1,6 @@
 require "embulk/input/bigquery/version"
 require "google/cloud/bigquery"
+require 'erb'
 
 module Embulk
   module Input
@@ -7,8 +8,27 @@ module Embulk
 			Plugin.register_input('bigquery', self)
 
 			def self.transaction(config, &control)
+				sql = config[:sql]
+				params = {}
+				unless sql
+					sql_erb = config[:sql_erb]
+					erb = ERB.new(sql_erb)
+					erb_params = config[:erb_params]
+					erb_params.each do |k, v|
+						params[k] = eval(v)
+					end
 
-				task = {project: config[:project], keyfile: config[:keyfile], sql: config[:sql], columns: config[:columns]}
+					sql = erb.result(binding)
+				end
+
+				task = {
+					project: config[:project],
+					keyfile: config[:keyfile],
+					sql: sql,
+					columns: config[:columns],
+					params: params
+				}
+
 				columns = []
 				config[:columns].each_with_index do |c, i|
 					columns << Column.new(i, c['name'], c['type'].to_sym)
@@ -21,12 +41,18 @@ module Embulk
 
 			def run
 				bq = Google::Cloud::Bigquery.new(project: @task[:project], keyfile: @task[:keyfile])
+				params = @task[:params]
 				rows = bq.query(@task[:sql])
 				rows.each do |row|
 					columns = []
 					@task[:columns].each do |c|
-						columns << row[c['name']]
+						val = row[c['name']]
+						if c['eval']
+							val = eval(c['eval'], binding)
+						end
+						columns << val
 					end
+
 					@page_builder.add(columns)
 				end
 				@page_builder.finish
